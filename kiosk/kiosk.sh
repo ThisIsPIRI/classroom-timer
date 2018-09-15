a93f61e1565d6551c0752c19420328bc058503ef
ThisIsPIRI <thisispiri@gmail.com>
2018-09-15 18:43:42 +0900

    Add date to downloaded files

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -1,4 +1,4 @@
-# Designed to run in Raspbian and from local.rc via xinit
+# Written for Raspbian. Usage: sudo xinit ./kiosk.sh
 sudo service ntp stop
 sudo ntpdate 0.kr.pool.ntp.org
 sudo service ntp start
@@ -6,10 +6,12 @@ sudo pkill python3
 sudo pkill python
 cd ct
 cd lib
-python3 downloadMenu.py "http://stu.sen.go.kr/sts_sci_md00_001.do?schulCode=B100000456&schulCrseScCode=3&SchulKndScCode=04&schYm=" menu.html
+# The resulting string below is the same as what AddDate in data.txt adds to the menuURL.
+yearMonth=$(date +%Y%m)
+python3 downloadPage.py "http://stu.sen.go.kr/sts_sci_md00_001.do?schulCode=B100000456&schulCrseScCode=3&SchulKndScCode=04&schYm=" menu$yearMonth
 cd ..
 unclutter &
 python3 -m http.server &
 su pi -c "chromium-browser --kiosk --incognito 0.0.0.0:8000/timer.html"
 
-#ONLY USE WHEN ABOVE METHOD IS UNAVAILABLE su pi -c chromium-browser /home/pi/ct/timer.html --disable-web-security --user-data-dir
+#su pi -c chromium-browser /home/pi/ct/timer.html --disable-web-security --user-data-dir --kiosk
